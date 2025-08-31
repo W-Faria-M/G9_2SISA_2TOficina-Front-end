86ab8b97a9a7ed90de086941940bb895fcf4426a
PedroVsantos <pedro.vsantos@sptech.school>
2025-08-31 20:47:02 -0300

    Add: Integração com json-server, inserção no banco mocado db.json e consulta na tentativa de login

--- g9-2sisa-front-end/src/App.jsx
+++ g9-2sisa-front-end/src/App.jsx
@@ -3,17 +3,17 @@ import Navbar from './components/navbar'
 import CadastroCliente from './pages/cadastroCliente'
 import LoginCliente from './pages/loginCliente'
 import Home from './pages/home'
-import LoginFuncionario from './pages/loginFuncionario';
-import RedirectMessage from './pages/redirect-message';
+// import LoginFuncionario from './pages/loginFuncionario';
+// import RedirectMessage from './pages/redirect-message';
 
 function App() {
 
   return (
     <>
     {/* <RedirectMessage />; */}
-    <LoginFuncionario />
-     {/* <Navbar /> */}
-      {/* <CadastroCliente /> */}
+    {/* <LoginFuncionario /> */}
+     <Navbar />
+      <CadastroCliente />
       {/* <LoginCliente /> */}
       {/* {<Home /> } */}
     </>
