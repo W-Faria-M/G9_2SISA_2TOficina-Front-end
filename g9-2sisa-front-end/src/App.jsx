19dd3493c3e9f426e5cfd68cef94e3a46fef960d
PedroVsantos <pedro.vsantos@sptech.school>
2025-09-07 22:31:04 -0300

    Primeira versão da tela de agendamentos

--- g9-2sisa-front-end/src/App.jsx
+++ g9-2sisa-front-end/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/navbar'
 import CadastroCliente from './pages/cadastroCliente'
 import LoginCliente from './pages/loginCliente'
 import Home from './pages/home'
+import AgendamentosFeitos from './pages/agendamentosFeitos'
 // import LoginFuncionario from './pages/loginFuncionario';
 // import RedirectMessage from './pages/redirect-message';
 
@@ -12,9 +13,10 @@ function App() {
     <>
     {/* <RedirectMessage />; */}
     {/* <LoginFuncionario /> */}
-     <Navbar />
-      <CadastroCliente />
+     {/* <Navbar /> */}
+      {/* <CadastroCliente /> */}
       {/* <LoginCliente /> */}
+      <AgendamentosFeitos />
       {/* {<Home /> } */}
     </>
   )
---
+++ g9-2sisa-front-end/src/components/filterBar.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { Search, Filter } from "lucide-react";
+import "./FilterBar.css";
+
+export default function FilterBar({ onSearch, onFilter }) {
+    const [search, setSearch] = useState("");
+
+    const handleSearch = (e) => {
+        setSearch(e.target.value);
+        if (onSearch) onSearch(e.target.value);
+    };
+
+    return (
+        <div className="filter-wrapper">
+            <div className="agendar-container">
+                <button className="agendar-button">+ Agendar</button>
+            </div>
+
+            <div className="filter-bar">
+                <div className="search-container">
+                    <Search className="search-icon" size={20} />
+                    <input
+                        type="text"
+                        value={search}
+                        onChange={handleSearch}
+                        placeholder="Pesquisar"
+                        className="search-input"
+                    />
+                </div>
+
+                <button onClick={onFilter} className="filter-button">
+                    <Filter size={18} />
+                    Filtrar
+                </button>
+            </div>
+        </div>
+    );
+}
